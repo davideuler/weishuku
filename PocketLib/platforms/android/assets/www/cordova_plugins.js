7293819b605603691e209cd324f4647265760927
zhaogang.lv <zhaogang.lv@dianping.com>
2013-12-23 19:47:04 +0800

    generated file in android/assets removed

--- PocketLib/platforms/android/assets/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "com.mirasense.scanditsdk.plugin": "1.1.0"
-}
-// BOTTOM OF METADATA
-});
